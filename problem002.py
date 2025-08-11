4c3f9fc256ec3e4c8e78629b622e186d3a90124f
mehedihasanuchee <mehedihasansuchee@outlook.com>
2025-08-11 06:27:57 +0600

    001

---
+++ problem002.py
@@ -0,0 +1,20 @@
+'''Given an integer, , perform the following conditional actions:
+
+If  is odd, print Weird
+If  is even and in the inclusive range of  to , print Not Weird
+If  is even and in the inclusive range of  to , print Weird
+If  is even and greater than , print Not Weird'''
+
+def test(n):
+    if n % 2 != 0:
+        print("Weird")
+    elif (n % 2 == 0) and (2 <= n <= 5):
+        print("Not Weird")
+    elif (n % 2 == 0) and (6 <= n <= 20):
+        print("Weird")
+    elif (n % 2 == 0) and (n > 20):
+        print("Not Weird")
+
+if __name__ == '__main__':
+    n = int(input(). strip())
+    test(n)
