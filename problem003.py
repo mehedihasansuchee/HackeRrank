5082e01433da470d3c848774475a5a318ba8090f
mehedihasanuchee <mehedihasansuchee@outlook.com>
2025-08-24 23:39:01 +0600

    Arithmetic Operators

--- problem003.py
+++ problem003.py
@@ -1,10 +1,5 @@
 #Arithmetic Operators
 
-'''The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
-
-The first line contains the sum of the two numbers.
-The second line contains the difference of the two numbers (first - second).
-The third line contains the product of the two numbers.'''
 
 def arithmetic_perators (num1, num2):
     print(num1+num2)
