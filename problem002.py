d53515124dd105d04dbf03c68f813ffde7feb727
mehedihasanuchee <mehedihasansuchee@outlook.com>
2025-08-24 23:38:24 +0600

    Python If-Else

--- problem002.py
+++ problem002.py
@@ -1,11 +1,5 @@
 #Python If-Else
 
-'''Given an integer, , perform the following conditional actions:
-
-If  is odd, print Weird
-If  is even and in the inclusive range of  to , print Not Weird
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird'''
 
 def test(n):
     if n % 2 != 0:
