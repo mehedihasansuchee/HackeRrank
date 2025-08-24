b2260cf014f9a0a9fb5254a0ade13ee1fb25fcac
mehedihasanuchee <mehedihasansuchee@outlook.com>
2025-08-24 23:41:46 +0600

    sWAP cASE

--- problem006.py
+++ problem006.py
@@ -1,26 +1,5 @@
 #sWAP cASE
 
-'''You are given a string and your task is to swap cases.
-In other words, convert all lowercase letters to uppercase letters and vice versa.'''
-
-'''def swap_case(s):
-    new_s = []
-
-    for item in s:
-        if item.isalpha():
-            if item.islower():
-                new_s.append(item.upper())
-            else:
-                new_s.append(item.lower())
-        else:
-            new_s.append(item)
-    return ''.join(new_s)
-    
-    #Execution part
-if __name__ == '__main__':
-    s = input()
-    result = swap_case(s)
-    print(result)'''
 
 def swap_case(s):
     return s.swapcase()
