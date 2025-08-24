9c262d0373f5f6cb743333b247eddf8e0c7f16fd
mehedihasanuchee <mehedihasansuchee@outlook.com>
2025-08-24 06:20:58 +0600

    Python: Division

--- problem004.py
+++ problem004.py
@@ -1,7 +1,9 @@
 #Python: Division
+
 '''The provided code stub reads two integers,  and , from STDIN.
 
-Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
+Add logic to print two lines. The first line should contain the result of integer
+division,  // . The second line should contain the result of float division,  / .
 
 No rounding or formatting is necessary.'''
 
