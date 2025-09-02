baed6de2975aa94c87732173c34fa6d923037e3f
mehedihasanuchee <mehedihasansuchee@outlook.com>
2025-09-02 06:20:18 +0600

    Say "Hello, World!" With Python

---
+++ problem017.py
@@ -0,0 +1,13 @@
+#Mutation
+
+def mutate_string(string, position, character):
+    my_list = list(string)
+    my_list[position] = character
+    final_list = ' '.join(my_list)
+    return final_list
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
