6321341f704f887a7d76ad4516d2066fccc8dff8
mehedihasanuchee <mehedihasansuchee@outlook.com>
2025-09-15 07:11:02 +0600

    Set Mutations

--- 037_Set Mutations.py
+++ 037_Set Mutations.py
@@ -1,7 +1,29 @@
 #Set Mutations
 
 
-def updateit(setA,s,command):
+len_a = int(input())
+a = set(map(int, input().split()))
+
+n = int(input())
+
+for _ in range(n):
+    operation = input().split()[0]
+    other_set = set(map(int, input().split()))
+
+    if operation == "update":
+        a.update(other_set)
+    elif operation == "intersection_update":
+        a.intersection_update(other_set)
+    elif operation == "difference_update":
+        a.difference_update(other_set)
+    elif operation == "symmetric_difference_update":
+        a.symmetric_difference_update(other_set)
+
+print(sum(a))
+
+
+
+'''def updateit(setA,s,command):
     if command == "update":
         setA.update(s)
     elif command == "difference_update":
@@ -19,4 +41,4 @@ for i in range(int(input())):
     command,len_of_set = input().split()
     s = set(map(int, input().split()))
     setA = updateit(setA,s,command)
-print(sum(setA))
+print(sum(setA))'''
