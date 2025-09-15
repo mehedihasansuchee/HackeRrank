250c15d16152fc60dad6b3ad05cad37c82e7a855
mehedihasanuchee <mehedihasansuchee@outlook.com>
2025-09-15 06:47:54 +0600

    Set .difference() Operation

--- 035_Set .difference() Operation.py
+++ 035_Set .difference() Operation.py
@@ -1,10 +1,12 @@
 #Set .difference() Operation
 
 
-eng = int(input())
-eng_roll = set(map(int, input().split()))
+n_english = int(input())
+sub_english = set(map(int, input().split()))
 
-french = int(input())
-french_roll = set(map(int, input().split()))
+n_french = int(input())
+sub_french = set(map(int, input().split()))
 
-print(len(eng_roll.difference(french_roll)))
+only_english = sub_english.difference(sub_french)
+
+print(len(only_english))
