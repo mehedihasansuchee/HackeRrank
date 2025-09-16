cde5e648b9ed5f90bf01c24a7b567aedf2cb1ea2
mehedihasanuchee <mehedihasansuchee@outlook.com>
2025-09-16 07:17:02 +0600

    itertools.product()

---
+++ 048_itertools.product().py
@@ -0,0 +1,10 @@
+#itertools.product()
+
+
+from itertools import product
+
+A = list(map(int,input().split()))
+B = list(map(int, input().split()))
+
+for i in list(product(A, B)):
+    print(i, end = ' ')
