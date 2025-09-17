50ab469aac6be48de1602b10509e89c09c844ad9
mehedihasanuchee <mehedihasansuchee@outlook.com>
2025-09-17 06:59:55 +0600

    Incorrect Regex

---
+++ 066_Incorrect Regex.py
@@ -0,0 +1,13 @@
+#Incorrect Regex
+
+
+import re
+
+t = input()
+for i in range(t):
+    reg = raw_input()
+    try:
+        re.compile(reg)
+        print(True)
+    except re.error:
+        print(False)
