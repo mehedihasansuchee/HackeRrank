a9d77dd8fe6d012d9b6ef4afcd5c7aa6cafbee0e
mehedihasanuchee <mehedihasansuchee@outlook.com>
2025-10-11 21:43:29 +0600

    HTML Parser - Part 1

--- 088_HTML Parser - Part 1.py
+++ 088_HTML Parser - Part 1.py
@@ -2,23 +2,3 @@
 
 
 from html.parser import HTMLParser
-
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print("Start    :",tag)
-        if attrs:
-            for each in attrs:
-                print('-> '+' > '. join(list(map(str, each))))
-
-    def handle_endtag(self, tag):
-        print("End      :",tag)
-
-    def handle_startendtag(self, tag, attrs):
-        print("Empty    :",tag)
-        if attrs:
-            for each in attrs:
-                print('-> '+' > '.join(list(map(str, each))))
-
-parser = MyHTMLParser()
-for each in range(int(input())):
-    parser.feed(input())
